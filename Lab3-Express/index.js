23662e9c3a730c27b57c6f846096d7053804f976
Santiago <santipr2004@gmail.com>
2023-09-11 12:04:55 -0500

    Feat: all complete

---
+++ Lab3-Express/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const cors = require('cors'); 
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let items = []
+
+app.get('/items', (req, res) => {  
+  res.send({"items":items})
+})
+
+app.post('/items', (req, res) => {
+  const newItem = {
+    x: req.body.x,
+    y: req.body.y,
+    r: req.body.r,
+    g: req.body.g,
+    b: req.body.b,
+  }
+
+  items.push(newItem);
+
+  res.send({"response":`New Item at ${newItem.x}, ${newItem.y}`});
+})
+
+app.get('/', (req, res) => {    
+  res.send('Server started')
+})
+
+app.get('/users', (req, res) => {
+  res.send({"users":users})
+})
+
+app.post('/clear', (req, res) => {    
+  items = []
+  res.send('Server cleared')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
